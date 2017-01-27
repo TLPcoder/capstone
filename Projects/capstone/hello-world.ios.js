7fbe25a8e18218a7db47c1692c86886f62e57932
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-01-26 19:01:05 -0800

    hello

---
+++ Projects/capstone/hello-world.ios.js
@@ -0,0 +1,13 @@
+"use strict";
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
