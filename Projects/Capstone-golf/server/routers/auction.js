12b5cc979ece8248aa895c5647949564fe9682fb
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-01-30 13:02:01 -0800

    you can search for auctions based on zip code and distance from that zip code

--- Projects/Capstone-golf/server/routers/auction.js
+++ Projects/Capstone-golf/server/routers/auction.js
@@ -2,16 +2,47 @@
 const express = require('express');
 const router = express.Router();
 var knex = require('../knex');
+var fetch = require('node-fetch');
 
-router.get('/', function(req,res){
-    knex.select('courses.name','courses.description','courses.city','courses.country','courses.state','auction.course_id', 'auction.tee_time', 'auction.auction_ends', 'auction.owner_id', 'users.username','bids.bider_id', 'bids.bid_amount')
-    .from('auction')
-    .innerJoin('courses', 'courses.id', 'auction.course_id')
-    .innerJoin('users' , 'users.id', 'auction.owner_id')
-    .innerJoin('bids', 'bids.auction_id', 'auction.id')
-    .then(function(data){
-        res.send(data);
-    });
+router.get('/', function(req, res) {
+    knex.select('courses.name', 'courses.description', 'courses.city', 'courses.country', 'courses.state', 'auction.course_id', 'auction.tee_time', 'auction.auction_ends', 'auction.owner_id', 'courses.image', 'users.username', 'bids.bider_id', 'bids.bid_amount')
+        .from('auction')
+        .innerJoin('courses', 'courses.id', 'auction.course_id')
+        .innerJoin('users', 'users.id', 'auction.owner_id')
+        .innerJoin('bids', 'bids.auction_id', 'auction.id')
+        .then(function(data) {
+            res.json(data);
+        });
+});
+//api.geonames.org/findNearbyPostalCodesJSON?postalcode=94030&country=US&radius=20&username=tlpcoder
+router.get('/:postalcode/:distance/', function(req, res) {
+    var params = req.params;
+    var zipCodeCity = [];
+    var url = `http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=${params.postalcode}&country=US&radius=${params.distance}&username=tlpcoder`;
+    fetch(url)
+        .then(data => {
+            var json = data.json();
+            return json;
+        })
+        .then(json => {
+            console.log("json", json);
+            json.postalCodes.forEach(function(city){
+                zipCodeCity.push(city.placeName);
+            });
+            console.log("zip codes", zipCodeCity);
+            knex.select('courses.name', 'courses.description', 'courses.city', 'courses.country','courses.zip', 'courses.state', 'auction.course_id', 'auction.tee_time', 'auction.auction_ends', 'auction.owner_id', 'courses.image', 'users.username', 'bids.bider_id', 'bids.bid_amount')
+                .from('auction')
+                .innerJoin('courses', 'courses.id', 'auction.course_id')
+                .innerJoin('users', 'users.id', 'auction.owner_id')
+                .innerJoin('bids', 'bids.auction_id', 'auction.id')
+                .whereIn('courses.city', zipCodeCity)
+                .then(function(data) {
+                    console.log("course data", data);
+                    res.json(data);
+                });
+        }).catch(function(err) {
+            console.log(err);
+        });
 });
 
 
